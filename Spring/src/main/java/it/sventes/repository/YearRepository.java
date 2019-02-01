7ed07ff3a7573c27fb4731a15f96ccb92718b79b
Litroom <39905214+Litroom@users.noreply.github.com>
2019-02-01 15:56:49 +0200

    Cleaning Project

--- Spring/src/main/java/it/sventes/repository/YearRepository.java
+++
@@ -1,7 +0,0 @@
-package it.sventes.repository;
-
-import it.sventes.model.year.Year;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface YearRepository  extends JpaRepository<Year, Long> {
-}

137e3b8764199e3216f4a40322d9164293ae7653
Litroom <39905214+Litroom@users.noreply.github.com>
2019-02-01 15:56:59 +0200

    Cleaning Project

--- Spring/src/main/java/it/sventes/repository/HolidaysRepository.java
+++
@@ -1,7 +0,0 @@
-package it.sventes.repository;
-
-import it.sventes.model.holidays.Holidays;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HolidaysRepository extends JpaRepository<Holidays, Long> {
-}
